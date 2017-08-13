d80019a09d46b76105ab7b91c7aa127425039d75
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-08-13 12:33:32 -0500

    Fix copyright text in footer.

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
+<?php
+/**
+ * Template for the site footer.
+ */
+?>
+
 </div><!-- #content -->
 
 <footer class="site-footer page__wrapper--inner">
-    <div class="site-copyright">
+	<div class="site-copyright">
 		<?php
-		esc_html_e( 'All Content &copy', 'jmw2016' );
-		esc_html( date( 'Y' ) . ' ' . get_bloginfo( 'name' ) );
+		esc_html_e( 'All Content &copy;', 'jmw2016' );
+		echo esc_html( date( 'Y' ) . ' ' . get_bloginfo( 'name' ) );
 		?>
-    </div>
+	</div>
 </footer>
 
 </div><!-- .page-wrapper -->
